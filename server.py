2ccbcc2b1c984000881b6c4a2e9d5d10adf4caf3
Carlo <carloaqui.8@gmail.com>
2025-01-27 01:14:26 -0500

    did something

--- server.py
+++ server.py
@@ -1,6 +1,28 @@
 import socket
 from http import server
 import requests
+import sys
+import argparse
+
+BUFF_LEN = 1024
 
 if __name__ == '__main__':
-    print('Hello world')
+    try:
+        HOST = '127.0.0.1'
+        PORT = 3000
+
+        s = socket.socket()
+        s.bind((HOST, PORT))
+        s.listen()
+
+        while True:
+            conn, addr = s.accept()
+            print(conn, addr)
+            data = conn.recv(BUFF_LEN)
+            if data:
+                print(data.decode())
+                conn.send('wahaha'.encode())
+
+    except Exception as e:
+        s.close()
+        print(e)
